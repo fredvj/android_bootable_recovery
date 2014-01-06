7c34e20b127b3ed8db9fef2a753f695f242334c0
Fred vom Jupiter <fredvjupiter@googlemail.com>
2014-01-06 13:39:31 +0100

    Changed swipe code to work with Huawei u8860 - before cleanup
    Change-Id: I43dd9473e281cebfebb5d1ba98ec89476a577e1d

--- swipe.c
+++ swipe.c
@@ -1,6 +1,3 @@
-static int in_touch = 0; // 1 = in a touch
-static int slide_right = 0;
-static int slide_left = 0;
 static int touch_x = 0;
 static int touch_y = 0;
 static int old_x = 0;
@@ -9,6 +6,7 @@ static int diff_x = 0;
 static int diff_y = 0;
 static int min_x_swipe_px = 100;
 static int min_y_swipe_px = 80;
+static long old_usec = 0L;
 
 static void set_min_swipe_lengths() {
     char value[PROPERTY_VALUE_MAX];
@@ -18,6 +16,10 @@ static void set_min_swipe_lengths() {
         min_x_swipe_px = (int)(0.5 * screen_density); // Roughly 0.5in
         min_y_swipe_px = (int)(0.3 * screen_density); // Roughly 0.3in
     }
+
+    // fprintf(stderr, "screen_density := %d\n", screen_density);
+    // fprintf(stderr, "min_x_swipe_px := %d\n", min_x_swipe_px);
+    // fprintf(stderr, "min_y_swipe_px := %d\n", min_y_swipe_px);
 }
 
 static void reset_gestures() {
@@ -36,61 +38,84 @@ void swipe_handle_input(int fd, struct input_event *ev) {
     int k;
     set_min_swipe_lengths();
 
-    ioctl(fd, EVIOCGABS(ABS_MT_POSITION_X), abs_store);
-    int max_x_touch = abs_store[2];
-
-    ioctl(fd, EVIOCGABS(ABS_MT_POSITION_Y), abs_store);
-    int max_y_touch = abs_store[2];
+    // fprintf(stderr, "ev->type  := %d\n", ev->type);
+    // fprintf(stderr, "ev->code  := %d\n", ev->code);
 
     if(ev->type == EV_ABS && ev->code == ABS_MT_TRACKING_ID) {
-        if(in_touch == 0) {
-            in_touch = 1;
-            reset_gestures();
-        } else { // finger lifted
-            ev->type = EV_KEY;
-            if(slide_right == 1) {
-                ev->code = KEY_POWER;
-                slide_right = 0;
-            } else if(slide_left == 1) {
-                ev->code = KEY_BACK;
-                slide_left = 0;
-            }
+	fprintf(stderr, "ABS_MT_TRACKING_ID\n");
+	fprintf(stderr, "ev->value := %d\n", ev->value);
 
-            ev->value = 1;
-            in_touch = 0;
-            reset_gestures();
-        }
+	if(old_usec != 0L) {
+	    if((ev->time.tv_sec*1000000L + ev->time.tv_usec) > (old_usec + 200000L)) {
+		reset_gestures();
+	    }
+	}
+
+	old_usec = ev->time.tv_sec*1000000L + ev->time.tv_usec;
+
     } else if(ev->type == EV_ABS && ev->code == ABS_MT_POSITION_X) {
+	fprintf(stderr, "ABS_MT_POSITION_X\n");
+
+	// ioctl(fd, EVIOCGABS(ABS_MT_POSITION_X), abs_store);
+	// int max_x_touch = abs_store[2];
+
         old_x = touch_x;
-        float touch_x_rel = (float)ev->value / (float)max_x_touch;
-        touch_x = touch_x_rel * gr_fb_width();
+        // float touch_x_rel = (float)ev->value / (float)max_x_touch;
+        // touch_x = touch_x_rel * gr_fb_width();
+	touch_x = ev->value;
 
         if(old_x != 0) diff_x += touch_x - old_x;
 
+	fprintf(stderr, "value  := %d\n", ev->value);
+	fprintf(stderr, "width  := %d\n", gr_fb_width());
+	fprintf(stderr, "diffx  := %d\n", diff_x);
+
         if(diff_x > min_x_swipe_px) {
-            slide_right = 1;
+            ev->type = EV_KEY;
+	    ev->code = KEY_POWER;
+	    ev->value = 1;
             reset_gestures();
         } else if(diff_x < -min_x_swipe_px) {
-            slide_left = 1;
+            ev->type = EV_KEY;
+ 	    ev->code = KEY_BACK;
+	    ev->value = 1;
             reset_gestures();
         }
     } else if(ev->type == EV_ABS && ev->code == ABS_MT_POSITION_Y) {
+	fprintf(stderr, "ABS_MT_POSITION_Y\n");
+
+	// ioctl(fd, EVIOCGABS(ABS_MT_POSITION_Y), abs_store);
+	// int max_y_touch = abs_store[2];
+
         old_y = touch_y;
-        float touch_y_rel = (float)ev->value / (float)max_y_touch;
-        touch_y = touch_y_rel * gr_fb_height();
+        // float touch_y_rel = (float)ev->value / (float)max_y_touch;
+        // touch_y = touch_y_rel * gr_fb_height();
+	touch_y = ev->value;
 
         if(old_y != 0) diff_y += touch_y - old_y;
 
+	fprintf(stderr, "value   := %d\n", ev->value);
+	fprintf(stderr, "height  := %d\n", gr_fb_height());
+	fprintf(stderr, "diff_y  := %d\n", diff_y);
+
         if(diff_y > min_y_swipe_px) {
-            ev->code = KEY_VOLUMEDOWN;
             ev->type = EV_KEY;
+            ev->code = KEY_VOLUMEDOWN;
+	    ev->value = 1;
             reset_gestures();
         } else if(diff_y < -min_y_swipe_px) {
-            ev->code = KEY_VOLUMEUP;
             ev->type = EV_KEY;
+            ev->code = KEY_VOLUMEUP;
+	    ev->value = 1;
             reset_gestures();
         }
     }
+
+//	else if(ev->type == EV_ABS) {
+//	fprintf(stderr, "EV_ABS with ev->code := 0x%x & ev->value := 0x%x\n", ev->code, ev->value);
+//    } else {
+//	fprintf(stderr, "ev->type := 0x%x & ev->code := 0x%x & ev->value := 0x%x\n", ev->type, ev->code, ev->value);
+//    }
 
     return;
 }
